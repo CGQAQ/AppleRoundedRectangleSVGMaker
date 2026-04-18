959d8260caab46688d924c3f05eeb6c366721e80
CGQAQ <m.jason.liu@outlook.com>
2026-04-18 23:20:17 +0800

    Initial Commit

---
+++ RoundedRectangle/RoundedRectangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoundedRectangleApp.swift
+//  RoundedRectangle
+//
+//  Created by Jason Leo on 2026/4/18.
+//
+
+import SwiftUI
+
+@main
+struct RoundedRectangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
